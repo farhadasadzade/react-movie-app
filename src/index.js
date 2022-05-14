501b9f3d8c07d8c92868ff9eb99911c3afcf3d40
Farhad Asadzade <f.esedzade9789@gmail.com>
2022-05-14 23:07:41 +0400

    added local storage

--- src/index.js
+++ src/index.js
@@ -6,14 +6,19 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import store from './redux/store';
+import store, { persistor } from './redux/store';
+
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
+
   </BrowserRouter>
 );
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,42 @@
 import { createStore, compose, applyMiddleware } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './reducers/'
 import thunk from 'redux-thunk'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import {
+    persistStore, persistReducer, FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-let store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(thunk))
-)
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default store;
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['watchList']
+}
+
+// let store = createStore(
+//     rootReducer,
+//     composeEnhancers(applyMiddleware(thunk))
+// )
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+let store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
+
+export let persistor = persistStore(store)
+export default store
